8c3c07a9b8a06f0538407f6d25fe67230a29c02f
Peacefire1 <ganzorigenkhtsogt@gmail.com>
2023-01-15 20:11:31 +0800

    While - Loop

---
+++ JS Loop/While Loop/main.js
@@ -0,0 +1,43 @@
+
+// 1. 10 хүртэлх натурал тоог хэвлэх программ бич
+
+let i = 1;
+
+while(i <= 10){
+    console.log(i);
+    i += 1
+}
+
+
+// 2. 10 хүртэлх натурал тооны сондгой тоонуудыг хэвлэх программ бич
+
+let a = Number(prompt("give me number 1 to 10"));
+while (a <= 10){
+    if(a % 2 == 1){
+        console.log(a + "-This sondgoi");
+    }else{
+        console.log(a + "-Tegsh");
+    }
+    a += 1
+}
+
+
+// 3. 10 хүртэлх натурал тооны тэгш тоонуудыг хэвлэх программ бич
+
+
+// Дээр хамт хийсэн
+
+
+
+// 4. 100 хүртэлх натурал тооны нийлбэрийг олох программ бич
+
+let naturalNumber = Number(prompt("Natural Number"))
+
+while (naturalNumber <= 100){
+    
+}
+
+
+
+
+
