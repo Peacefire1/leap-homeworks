eb17baf7b80c1b4141dd858942538b5b1a7cb95b
Peacefire1 <ganzorigenkhtsogt@gmail.com>
2023-01-25 22:13:37 +0800

    2 array urjver

---
+++ JS-Array/хоёр массивын үржвэр/main.js
@@ -0,0 +1,9 @@
+let arr1 = [3, 45, 23, 78, 34];
+let arr2 = [4, 2, 34, 4, 12, 1];
+
+for (let i = 0; i < arr1.length; i++) {
+  let Arr1 = arr1[i];
+  let Arr2 = arr2[i];
+  let arr3 = Arr1 * Arr2;
+  console.log(arr3);
+}
